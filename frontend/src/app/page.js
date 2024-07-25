d061efecaa9a53b477e1c154ab041e2f5f521e1f
teacher-cheater <johnnykayfun@gmail.com>
2024-07-25 21:00:06 +0300

    add: cors, output of car brands

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,30 @@
 "use client";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
-import { Table, Select, Pagination } from "antd";
-
+import { Table, Select, Pagination, message } from "antd";
+import axios from "axios";
 const { Option } = Select;
 
 export default function Home() {
+  // const [models, setModels] = useState([]);
+  const [brands, setBrands] = useState([]);
+
+  // console.log("models", models);
+  console.log("brands", brands);
+
+  useEffect(() => {
+    fetchBrands();
+  }, []);
+
+  const fetchBrands = async () => {
+    try {
+      const response = await axios.get("http://localhost:4444/api/brands");
+      setBrands(response.data);
+    } catch (error) {
+      message.error("Ошибка при загрузке списка марок");
+    }
+  };
+
   const columns = [
     { title: "ID", dataIndex: "_id", key: "_id" },
     { title: "Марка", dataIndex: "mark", key: "mark" },
