d27abfafddee1c696b7df2665fc4bfca239a7bd7
teacher-cheater <johnnykayfun@gmail.com>
2024-07-25 23:30:56 +0300

    add: brand selection, selection of multiple model options, rendering selection

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -6,15 +6,25 @@ import axios from "axios";
 const { Option } = Select;
 
 export default function Home() {
-  // const [models, setModels] = useState([]);
+  const [models, setModels] = useState([]);
   const [brands, setBrands] = useState([]);
+  const [selectedBrand, setSelectedBrand] = useState(null);
+  const [selectedModels, setSelectedModels] = useState([]);
+  const [stock, setStock] = useState([]);
 
-  // console.log("models", models);
-  console.log("brands", brands);
+  console.log("selectedModels", selectedModels);
+  // console.log("brands", brands);
 
   useEffect(() => {
     fetchBrands();
   }, []);
+
+  useEffect(() => {
+    if (selectedBrand) {
+      fetchModels(selectedBrand);
+      fetchStock(selectedBrand, selectedModels);
+    }
+  }, [selectedBrand, selectedModels]);
 
   const fetchBrands = async () => {
     try {
@@ -22,6 +32,30 @@ export default function Home() {
       setBrands(response.data);
     } catch (error) {
       message.error("Ошибка при загрузке списка марок");
+    }
+  };
+
+  const fetchModels = async brand => {
+    console.log("brand--", brand);
+    try {
+      const response = await axios.get(
+        `http://localhost:4444/api/models/${brand}`
+      );
+      console.log("response", response.data);
+      setModels(response.data);
+    } catch (error) {
+      message.error("Ошибка при загрузке моделей");
+    }
+  };
+
+  const fetchStock = async (brand, models) => {
+    try {
+      const response = await axios.get("http://localhost:4444/api/stock", {
+        params: { mark: brand, models: models.join(",") },
+      });
+      setStock(response.data);
+    } catch (error) {
+      message.error("Ошибка при загрузке стока");
     }
   };
 
@@ -47,14 +81,54 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
+      {/* <div style={{}}>
+        {brands.map(brand => (
+          <span
+            onClick={value => setSelectedBrand(value)}
+            style={{
+              display: "inlineBlock",
+              color: "blue",
+              marginRight: "12px",
+            }}
+            key={brand.count}
+          >
+            {brand.mark} {brand.count}{" "}
+          </span>
+        ))}
+      </div> */}
+
+      <p>Модель:</p>
+      <Select
+        placeholder="Выберите марку"
+        style={{ width: 200 }}
+        onChange={brand => setSelectedBrand(brand)}
+      >
+        {brands.map(brand => (
+          <Option key={brand.mark} value={brand.mark}>
+            {brand.mark} ({brand.count})
+          </Option>
+        ))}
+      </Select>
       <Select
         mode="multiple"
         placeholder="Выберите модели"
         style={{ width: 300, marginLeft: 20 }}
+        onChange={value => setSelectedModels(value)}
+        disabled={!selectedBrand}
       >
-        <Option key="model" value="model"></Option>
+        {models.map(model => (
+          <Option key={model} value={model}>
+            {model}
+          </Option>
+        ))}
       </Select>
-      <Table columns={columns} rowKey="_id" style={{ marginTop: 20 }} />
+      <Table
+        columns={columns}
+        dataSource={stock}
+        pagination={false}
+        rowKey="_id"
+        style={{ marginTop: 20 }}
+      />
       <Pagination style={{ marginTop: 20 }} />
     </main>
   );
